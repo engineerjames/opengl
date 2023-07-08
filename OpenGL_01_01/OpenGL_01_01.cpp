a6675a89ae2cd9e95ecaa955127e349e42f1b519
James Armes <jamesleearmes@gmail.com>
2023-07-08 13:54:10 -0600

    Initial upload

---
+++ OpenGL_01_01/OpenGL_01_01.cpp
@@ -0,0 +1,53 @@
+﻿// OpenGL_01_01.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learning OpenGL", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create the GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// Callbacks must be registered prior to being in the render loop
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
